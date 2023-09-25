85119f09e0e620c5ff54adc1d5bd31f8c4956501
a1b2c3d4efgh5 <144240263+a1b2c3d4efgh5@users.noreply.github.com>
2023-09-25 18:43:21 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,22 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+# Define the function
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
 
+# Create a list of products
+products = ["Apple", "Banana", "Orange", "Apple", "Mango", "Apple"]
 
-number = int(input("Enter a value:"))
-res = fact_rec(number)
+# Define the target product you want to search for
+target = "Apple"
 
-print("the factorial of {} is {}.".format(number, res))
+# Call the function to search for the target product in the list
+result = linear_search_product(products, target)
+
+# Print the result
+if result:
+    print(f"The target product '{target}' was found at indices: {result}")
+else:
+    print(f"The target product '{target}' was not found in the list.")
